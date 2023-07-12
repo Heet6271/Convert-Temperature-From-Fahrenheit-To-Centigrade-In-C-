2b2d05b22f046b08f607a6b81b720b6afbd9d64a
Heet Thummar <thummarhit6271@gmail.com>
2023-07-12 19:53:28 +0530

    cp2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prac2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            double fahrenheit = 100;
+            Console.WriteLine("Fahrenheit:" + fahrenheit);
+            celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("Celsius:" + celsius);
+            Console.ReadLine();
+        }
+    }
+}
